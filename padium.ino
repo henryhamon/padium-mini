2fe8eafb5ceed9baff6cb7f95d0c72f22aa72400
Henry Hamon <henry@hamonrails.com.br>
2025-02-14 19:36:21 -0300

    🎉 Initial Setup

---
+++ padium.ino
@@ -0,0 +1,13 @@
+/// Esp32 version
+
+#include "commons.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
